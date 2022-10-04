9e06b7e14fbea71c8be334df1e32e89b7320cd5f
kamiada <kamiada528@gmail.com>
2022-10-04 16:01:03 +0100

    adding fetch

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import axios from 'axios';
 
 const app = createApp(App)
+
+
 
 app.use(createPinia())
 app.use(router)
